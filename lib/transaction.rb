d27a462a9f4dbbcc8ec98216a910f5f5a3c199d7
Serena Bertozzi <serenabertozzi@protonmail.com>
2021-11-23 15:23:45 +0000

    create and test transaction class

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -7,4 +7,20 @@ class Transaction
     @current_balance = 0
     @is_credit = false
   end
+
+  def deposit(amount, date)
+    @current_balance += amount
+    @is_credit = true
+    save_transaction(amount, date)
+  end
+
+  def withdraw(amount, date)
+    @current_balance -= amount
+  end
+
+  private
+
+  def save_transaction(amount, date)
+    @current_transaction = { credit: @is_credit, amount: amount, date: date }
+  end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,4 +1,32 @@
 require "transaction"
 
 describe Transaction do
+  describe "#deposit" do
+    it { is_expected.to respond_to :deposit }
+
+    it "adds the deposited amount to the current balance" do
+      subject.deposit(1000, nil)
+      expect(subject.current_balance).to eq 1000
+    end
+
+    it "is a credit transaction" do
+      subject.deposit(1000, nil)
+      expect(subject.is_credit).to be true
+    end
+
+    it "saves the current deposit transaction" do
+      subject.deposit(1000, "16/08/2021")
+      expect(subject.current_transaction).to eq({ credit: true, amount: 1000, date: "16/08/2021" })
+    end
+  end
+
+  describe "#withdraw" do
+    it { is_expected.to respond_to :withdraw }
+
+    it "subtracts the withdrawed amount to the current balance" do
+      subject.current_balance = 1000
+      subject.withdraw(500, nil)
+      expect(subject.current_balance).to eq 500
+    end
+  end
 end
